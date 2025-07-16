8031998bd11545e61c5879ae7da5e65eee172bd1
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-16 17:25:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+
+	let table = document.getElementById('sampleTable');
+
+	let newRow = document.createElement('tr');
+	newRow.innerHTML = `
+	<td>New Cell1</td>
+	<td>New Cell2</td>
+	`
+	table.append(newRow);
 }
